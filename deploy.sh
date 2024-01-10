beb4aab96c23fb3d8a8ebc515269d343085e7055
RichardJi1 <jirichard2012@gmail.com>
2024-01-10 11:57:45 +1100

    added CNAME

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f https://github.com/RJi26/PersonalWebsite main
+git push -f git@github.com:RJi26/PersonalWebsite.git main:gh-pages
 
 cd -
